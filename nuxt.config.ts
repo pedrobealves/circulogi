f0704d3b8ec3d959fbef68d1adc4eb1ca1b74d38
pedrobealves <pedrobealves@gmail.com>
2024-11-21 02:05:28 -0300

    Remove launch configuration, update Nuxt compatibility date, and adjust package dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2024-04-03",
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
 
   future: {
     compatibilityVersion: 4,
   },
-
   extends: ["app/auth", "app/simulation", "app/website", "app/common"],
-  modules: ["@pinia/nuxt", "nuxt-workers", "@nuxtjs/tailwindcss"],
+  modules: ["@pinia/nuxt", "@nuxtjs/tailwindcss"],
   pinia: {
     storesDirs: ["./stores/**"],
   },
